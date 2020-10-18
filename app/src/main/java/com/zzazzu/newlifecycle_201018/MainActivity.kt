57ca442a42398057d63d26847569f2ed2261e88d
bucephalus41 <bucephalus41@gmail.com>
2020-10-19 01:39:40 +0900

    이거 왜 한거지

--- app/src/main/java/com/zzazzu/newlifecycle_201018/MainActivity.kt
+++ app/src/main/java/com/zzazzu/newlifecycle_201018/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.zzazzu.newlifecycle_201018
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("메인화면", "onCreate 실행됨")
+
+        moveToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("메인화면", "onResume 실행됨")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("메인화면", "onPause 실행됨")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("메인화면", "onDestroy 실행됨")
+    }
+
 }
---
+++ app/src/main/java/com/zzazzu/newlifecycle_201018/OtherActivity.kt
@@ -0,0 +1,25 @@
+package com.zzazzu.newlifecycle_201018
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_other.*
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        Log.d("다른 화면", "onCreate 실행됨")
+
+        goBackBtn.setOnClickListener {
+            finish()
+
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("다른화면", "onDestroy 실행됨")
+    }
+}
